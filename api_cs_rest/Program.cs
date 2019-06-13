feaa3871ac4218029fb6d9c14b661c8514fe37c8
Armando Cardenas <armando.cardenas@cetys.edu.mx>
2019-06-12 20:34:39 -0700

    Update Startup

--- api_cs_rest/Program.cs
+++ api_cs_rest/Program.cs
@@ -14,7 +14,15 @@ namespace api_cs_rest
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            // CreateWebHostBuilder(args).Build().Run();
+
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseStartup<Startup>()
+                .UseUrls("http://0.0.0.0:5000")
+                .Build();
+
+            host.Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
